040d600e0a81d0af1931dc7599d3f5079567f778
Alexandre Cumbane <Alexandrepcumbane@gmail.com>
2022-07-14 16:49:32 +0200

    fix not renderig images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  images: {
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
+  },
 }
-
-module.exports = nextConfig
